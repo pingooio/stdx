a55aa29ada5c017bab82828be27b98cf1fb7e66e
git <>
2024-08-17 10:04:42 +0000

    m

--- slicesx/unique.go
+++ slicesx/unique.go
@@ -1,12 +1,13 @@
 package slicesx
 
 func Unique[T comparable](input []T) []T {
-	inResult := make(map[T]bool, len(input))
-	var result []T
+	inResult := make(map[T]struct{}, len(input))
+	// we try to reduce allocations by using a default capacity
+	result := make([]T, 0, len(input)/4)
 
 	for _, elem := range input {
 		if _, ok := inResult[elem]; !ok {
-			inResult[elem] = true
+			inResult[elem] = struct{}{}
 			result = append(result, elem)
 		}
 	}
