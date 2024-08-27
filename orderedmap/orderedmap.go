5a44474793ec038456a59fb59b1cbefa257c3368
git <>
2024-08-27 07:21:06 +0000

    m

--- orderedmap/orderedmap.go
+++ orderedmap/orderedmap.go
@@ -3,10 +3,13 @@ package orderedmap
 import (
 	"bytes"
 	"encoding/json"
+	"iter"
 	"sort"
+
+	"github.com/pingooio/stdx/yaml"
 )
 
-type Element[K comparable, V any] struct {
+type Item[K comparable, V any] struct {
 	Key   K
 	Value V
 }
@@ -14,26 +17,62 @@ type Element[K comparable, V any] struct {
 // Map is wrapper for map that keeps it's order when deserializing from JSON
 // Warning: it's highly inneficient and should only be used for configuration file or similar use cases
 type Map[K comparable, V any] struct {
-	elems []Element[K, V]
+	items []Item[K, V]
 	data  map[K]V
 }
 
-func (m *Map[K, V]) UnmarshalJSON(data []byte) error {
-	json.Unmarshal(data, &m.data)
+func (m *Map[K, V]) UnmarshalJSON(data []byte) (err error) {
+	err = json.Unmarshal(data, &m.data)
+	if err != nil {
+		return
+	}
 
-	m.elems = make([]Element[K, V], 0, len(m.data))
+	m.items = make([]Item[K, V], 0, len(m.data))
 
 	index := make(map[K]int)
 	for key, value := range m.data {
-		m.elems = append(m.elems, Element[K, V]{Key: key, Value: value})
+		m.items = append(m.items, Item[K, V]{Key: key, Value: value})
 		esc, _ := json.Marshal(key) //Escape the key
 		index[key] = bytes.Index(data, esc)
 	}
 
-	sort.Slice(m.elems, func(i, j int) bool { return index[m.elems[i].Key] < index[m.elems[j].Key] })
+	sort.Slice(m.items, func(i, j int) bool { return index[m.items[i].Key] < index[m.items[j].Key] })
 	return nil
 }
 
-func (m *Map[K, V]) Elems() []Element[K, V] {
-	return m.elems
+func (m *Map[K, V]) UnmarshalYAML(yamlNode *yaml.Node) (err error) {
+	err = yamlNode.Decode(&m.data)
+	if err != nil {
+		return
+	}
+
+	m.items = make([]Item[K, V], 0, len(m.data))
+
+	index := make(map[K]int)
+	for key, value := range m.data {
+		m.items = append(m.items, Item[K, V]{Key: key, Value: value})
+		esc, _ := yaml.Marshal(key) //Escape the key
+		index[key] = bytes.Index([]byte(yamlNode.Value), esc)
+	}
+
+	sort.Slice(m.items, func(i, j int) bool { return index[m.items[i].Key] < index[m.items[j].Key] })
+	return nil
+}
+
+func (m *Map[K, V]) Items() []Item[K, V] {
+	return m.items
+}
+
+func (m *Map[K, V]) Len() int {
+	return len(m.items)
+}
+
+func (m *Map[K, V]) Iter() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for _, item := range m.items {
+			if !yield(item.Key, item.Value) {
+				return
+			}
+		}
+	}
 }
